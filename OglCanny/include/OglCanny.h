34503fda2abdd324c9c8bee4c258b8d2c5fac16d
Prem Sasidharan <prem.sasidharan@gmail.com>
2016-12-19 07:33:01 -0800

    Update OglCanny.h
    corrected spacing

--- OglCanny/include/OglCanny.h
+++ OglCanny/include/OglCanny.h
@@ -12,11 +12,12 @@ class Canny
 {
 public:
     static void init();
-	static void process(Ogl::Image<GL_RED>& destImg, const Ogl::Image<GL_RED>& srcImg, GLfloat minThresh, GLfloat maxThresh);
+
+    static void process(Ogl::Image<GL_RED>& destImg, const Ogl::Image<GL_RED>& srcImg, GLfloat minThresh, GLfloat maxThresh);
     static void process(Ogl::Image<GL_RED>& destImg, const Ogl::Image<GL_LUMINANCE>& srcImg, GLfloat minThresh, GLfloat maxThresh);
 
-protected:
-	static std::unique_ptr<Ogl::CannyPrv> mPrv;
+private:
+    static std::unique_ptr<Ogl::CannyPrv> mPrv;
 };
 
 };
