92a6a319429bd2966cb0e8ebafa9951e5174b989
PREM SASIDHARAN <prem.sasidharan@gmail.com>
2016-12-20 10:50:36 -0800

    Removing tabs

--- Tests/EdgeTest/include/GlView.h
+++ Tests/EdgeTest/include/GlView.h
@@ -6,24 +6,24 @@
 class GlView
 {
 public:
-	enum KEYS { LEFT, RIGHT, UP, DOWN };
+    enum KEYS { LEFT, RIGHT, UP, DOWN };
 
-	GlView(GLsizei w, GLsizei h);
-	~GlView();
+    GlView(GLsizei w, GLsizei h);
+    ~GlView();
 
 public:
-	void draw(uint8_t* pData);
-	void resize(GLsizei w, GLsizei h);
+    void draw(uint8_t* pData);
+    void resize(GLsizei w, GLsizei h);
 
-	void keyDown(KEYS key);
+    void keyDown(KEYS key);
 
 private:
     GLfloat minThresh;
     GLfloat maxThresh;
 
-	Ogl::Yuv420Image mYuvImg;
-	Ogl::Image<GL_RED> mEdgeImg;
+    Ogl::Yuv420Image mYuvImg;
+    Ogl::Image<GL_RED> mEdgeImg;
 
-	Ogl::ImagePainter< Ogl::Yuv420Shader, Ogl::Yuv420Image > mYuvPainter;
-	Ogl::ImagePainter< Ogl::LumaShader, Ogl::Image<GL_RED> > mLumaPainter;
+    Ogl::ImagePainter< Ogl::Yuv420Shader, Ogl::Yuv420Image > mYuvPainter;
+    Ogl::ImagePainter< Ogl::LumaShader, Ogl::Image<GL_RED> > mLumaPainter;
 };
--- Tests/EdgeTest/include/TestView.h
+++ Tests/EdgeTest/include/TestView.h
@@ -17,36 +17,36 @@ class TestView : public CWnd
 {
 // Construction
 public:
-	TestView(int w, int h, const char* fpath);
-	virtual ~TestView();
+    TestView(int w, int h, const char* fpath);
+    virtual ~TestView();
 
 // Attributes
 public:
 
 protected:
-	// Overrides
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+    // Overrides
+    virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 
-	// Generated message map functions
-	afx_msg void OnPaint();
-	afx_msg void OnDestroy();
-	afx_msg void OnTimer(UINT_PTR nIDEvent);
-	afx_msg void OnSize(UINT nType, int cx, int cy);
-	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
-	DECLARE_MESSAGE_MAP()
-
-private:
-	void initGL();
-	void createOpenGLContext();
+    // Generated message map functions
+    afx_msg void OnPaint();
+    afx_msg void OnDestroy();
+    afx_msg void OnTimer(UINT_PTR nIDEvent);
+    afx_msg void OnSize(UINT nType, int cx, int cy);
+    afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+    afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+    DECLARE_MESSAGE_MAP()
 
 private:
-	FILE* mFile;
-	size_t mWidth;
-	size_t mHeight;
-	size_t mFrameSize;
+    void initGL();
+    void createOpenGLContext();
 
-	std::vector<BYTE> mYuvData;
-	std::unique_ptr<GlView> mGlView;
-	std::unique_ptr<Ogl::WinGlContext> mGlCtxt;
+private:
+    FILE* mFile;
+    size_t mWidth;
+    size_t mHeight;
+    size_t mFrameSize;
+
+    std::vector<BYTE> mYuvData;
+    std::unique_ptr<GlView> mGlView;
+    std::unique_ptr<Ogl::WinGlContext> mGlCtxt;
 };
