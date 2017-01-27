63ed121b55364a46de14a596bdeba51cff902036
PREM SASIDHARAN <prem.sasidharan@gmail.com>
2017-01-26 20:00:06 -0800

    optimized

--- Tests/cl2scan/main.cpp
+++ Tests/cl2scan/main.cpp
@@ -1,27 +1,74 @@
 #include <iostream>
 #include <sstream>
 
-#include <CL/cl.hpp>
 #include <CL/cl2.hpp>
 
 #define OCL_PROGRAM_SOURCE(s) #s
 
 const char sSource[] = OCL_PROGRAM_SOURCE(
 
-kernel void child_scan(global int* pdata, int count)
+kernel void child_scan(global int4* pdata, int count)
 {
-    int i = get_global_id(0);
-    int data = work_group_scan_inclusive_add((i < count)?pdata[i]:0);
-    if (i < count) pdata[i] = data;
+    local int shData[1024];
+    int id = get_global_id(0);
+    ((int4 *)shData)[get_local_id(0)] = pdata[id];
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    int i = get_local_id(0);
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    int data = work_group_scan_inclusive_add((i<1024)?shData[i]:0);
+    if (i < 1024) shData[i] = data;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (id < count) pdata[id] = ((int4 *)shData)[get_local_id(0)];
 }
 
 kernel void gather_scan(global const int* pdata, global int* tempData, int offset, int count)
 {
-    int i = offset+(get_local_size(0)*get_local_id(0))-1;
-    tempData[get_local_id(0)] = work_group_scan_inclusive_add((i<count)?pdata[i]:0);
+    local int shData[1024];
+    for (int i = 0; i < 4; i++)
+    {
+        int index = (4*get_local_id(0))+i;
+        int j = offset+(1024*index)-1;
+        shData[index] = (j < count)?pdata[j]:0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    int i = get_local_id(0);
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    shData[i] = work_group_scan_inclusive_add(shData[i]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    i += 255;
+    int data = work_group_scan_inclusive_add((i<1024) ? shData[i] : 0);
+    if (i < 1024) shData[i] = data;
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int i = 0; i < 4; i++)
+    {
+        int index = (4*get_local_id(0))+i;
+        tempData[index] = shData[index];
+    }
 }
 
-kernel void add_data(global int* pdata, global const int* tempData, int offset, int count)
+kernel void add_data(global int4* pdata, global const int* tempData, int offset, int count)
 {
     local int shData;
     if (get_local_id(0) == 0)
@@ -29,8 +76,10 @@ kernel void add_data(global int* pdata, global const int* tempData, int offset, 
         shData = tempData[get_group_id(0)];
     }
     barrier(CLK_LOCAL_MEM_FENCE);
-    int i = offset+get_global_id(0);
-    if (i < count) pdata[i] += shData;
+    int i = (256*get_group_id(0))+get_local_id(0)+(offset/4);
+    int4 data = pdata[i];
+    data += shData;
+    pdata[i] = data;
 }
 
 kernel void scan(global int* pdata, global int* tempData, int count)
@@ -39,11 +88,12 @@ kernel void scan(global int* pdata, global int* tempData, int count)
     clk_event_t event2;
     const int BLK_SIZE = 256;
     const int BLK_SIZE2 = (BLK_SIZE*BLK_SIZE);
-    enqueue_kernel(get_default_queue(), CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange_1D(count, BLK_SIZE), 0, 0, &event1, ^{child_scan(pdata, count);});
-    for (int i = BLK_SIZE; i < count; i += BLK_SIZE2)
+    int ncount = count/4;
+    enqueue_kernel(get_default_queue(), CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange_1D(ncount, BLK_SIZE), 0, 0, &event1, ^{child_scan(pdata, ncount);});
+    for (int i = (4*BLK_SIZE); i < count; i += (16*BLK_SIZE2))
     {
         enqueue_kernel(get_default_queue(), CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange_1D(BLK_SIZE, BLK_SIZE), 1, &event1, &event2, ^{ gather_scan(pdata, tempData, i, count); });
-        enqueue_kernel(get_default_queue(), CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange_1D(BLK_SIZE2, BLK_SIZE), 1, &event2, &event1, ^{ add_data(pdata, tempData, i, count); });
+        enqueue_kernel(get_default_queue(), CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange_1D((4*BLK_SIZE2), BLK_SIZE), 1, &event2, &event1, ^{ add_data(pdata, tempData, i, count); });
     }
     release_event(event1);
 }
@@ -86,21 +136,17 @@ int main(int argc, char** argv)
         std::ostringstream options;
         options << "-cl-std=CL2.0";
 
-        cl::Program::Sources source(1, std::make_pair(sSource, strlen(sSource)));
-        cl::Program program(context, source);
+        cl::Program program(context, std::string(sSource));
         program.build(options.str().c_str());
 
         int count = 640*480;
-        cl_int* pIntData = (cl_int *)clSVMAlloc(context(), CL_MEM_READ_WRITE, 256*sizeof(cl_int), sizeof(cl_long));
-        cl_int* pInpData = (cl_int *)clSVMAlloc(context(), CL_MEM_READ_WRITE, count*sizeof(cl_int), sizeof(cl_long));
-        for (int i = 0; i < count; i++)
-        {
-            pInpData[i] = 1;
-        }
+        cl::SVMAllocator<int, cl::SVMTraitFine<>> svmAlloc(context);
+        std::vector<cl_int, cl::SVMAllocator<cl_int, cl::SVMTraitFine<>>> tempData(1024, 0, svmAlloc);
+        std::vector<cl_int, cl::SVMAllocator<cl_int, cl::SVMTraitFine<>>> inpData(count, 1, svmAlloc);
 
         cl::Kernel kernel(program, "scan");
-        clSetKernelArgSVMPointer(kernel(), 0, pInpData);
-        clSetKernelArgSVMPointer(kernel(), 1, pIntData);
+        kernel.setArg(0, inpData.data());
+        kernel.setArg(1, tempData.data());
         kernel.setArg(2, count);
         cl::Event event;
         queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(1), cl::NullRange, NULL, &event);
@@ -108,10 +154,16 @@ int main(int argc, char** argv)
         size_t time = (event.getProfilingInfo<CL_PROFILING_COMMAND_END>()-event.getProfilingInfo<CL_PROFILING_COMMAND_START>());
         printf("\nTime: %d ns", time);
 
-        clSVMFree(context(), pIntData);
-        pIntData = 0;
-        clSVMFree(context(), pInpData);
-        pInpData = 0;
+        bool flag = true;
+        for (size_t i = 0; i < inpData.size() && flag; i++)
+        {
+            if (inpData[i] != (i+1))
+            {
+                printf("\nFailed %d %d", inpData[i], i+1);
+                flag = false;
+            }
+        }
+        printf("\nPrfix sum %s", (flag ? "success" : "failed"));
     }
 
     catch (cl::Error error)
